b19a4cb51b52297d5250eff3d22fb70d06ecc6ac
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2025-11-19 17:50:38 +0900

    feat: Validate user age when creating or updating

--- app/schemas.py
+++ app/schemas.py
@@ -1,14 +1,16 @@
-from pydantic import BaseModel, EmailStr, ConfigDict
+from pydantic import BaseModel, EmailStr, Field, ConfigDict
 from typing import Optional
 
 class UserCreate(BaseModel):
     name: str
     email: EmailStr  # A Pydantic class for Email validation
-    age: int
+    age: int = Field(ge=0, le=150)  # 0 <= age <= 150
+
 
 class UserUpdate(BaseModel):
     name: Optional[str] = None
-    age: Optional[int] = None
+    age: Optional[int] = Field(default=None, ge=0, le=150)  # Same rule applies when updating
+
 
 class User(UserCreate):
     id: int
